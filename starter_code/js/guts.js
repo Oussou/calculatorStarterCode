dc7aaa45c8ab5b6634e89c2f05b910ea672fe734
oussou <ousmanetoure@claremontihs.org>
2016-10-20 21:50:16 +0000

    this is my calculator

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -2,3 +2,36 @@
 // Your functions must be called the following:
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
 
+function multiply(number1, number2){
+    return number1 * number2
+}
+
+function divide(number1, number2){
+     return number1 / number2
+}
+ 
+function subtract(number1, number2){
+    
+    return number1 - number2
+}
+
+function add(number1, number2){
+    return number1 + number2
+}
+
+function power(number1, number2){
+    return number1 ^ number2
+}
+
+function logBase10(number){
+    return Math.log10(number)
+}
+function generateRandomNumber(){
+    return Math.round (Math.random() * 10) 
+    
+    
+}
+
+function square_root(number){
+    return Math.sqrt(number)
+}
